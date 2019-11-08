69f7678179434f4e7325eff3306922dbc4ab79c9
dimalenser <dima.neuron@gmail.com>
2019-11-08 20:05:02 +0200

    половина макету програми готово
    Потрібно дописати:
    -рейтинг
    -занесення даних в базу даних(для початку це будуть файли)
    -обробники переривань

--- proekt_v_konsoli/proekt_v_konsoli/Program.cs
+++ proekt_v_konsoli/proekt_v_konsoli/Program.cs
@@ -6,6 +6,8 @@ namespace proekt_v_konsoli
     {
         static void test()
         {
+            string vidpovid;
+            int bal = 0;
             Console.WriteLine("Введіть ваше прізвище, ім'я, по батькові");
             string pib =  (Console.ReadLine());
             Console.WriteLine("Введіть номер вашої групи");
@@ -13,20 +15,44 @@ namespace proekt_v_konsoli
             Console.WriteLine(pib + grupa);
 
             //----------------------------
-            Console.WriteLine("Питання 1\n скільки буде 2+2*2?");
-            Console.WriteLine("a)0\n b)1\n c)2\nd)6 ");
-            string vidpovid = (Console.ReadLine());
-            int bal = 0;
-            if (vidpovid == "d") vidpovid += 1;
-            Console.WriteLine(vidpovid);
+            Console.WriteLine("Питання 1\n скільки буде 1+1*1?");
+            Console.WriteLine("a)0\nb)1\nc)2\nd)6 ");
+            vidpovid = (Console.ReadLine());
+            if (vidpovid == "c") bal += 1;
+            Console.WriteLine(bal);
+
+            //----------------------------
+            Console.WriteLine("\nПитання 2\n скільки буде 2+2*2?");
+            Console.WriteLine("a)0\nb)1\nc)2\nd)6 ");
+            vidpovid = (Console.ReadLine());
+            if (vidpovid == "d") bal += 1;
+            Console.WriteLine(bal);
+
+            //----------------------------
+            Console.WriteLine("\nПитання 3\n скільки буде 3+3*3?");
+            Console.WriteLine("a)12\nb)3\nc)9\nd)6 ");
+            vidpovid = (Console.ReadLine());
+            if (vidpovid == "a") bal += 1;
+            Console.WriteLine(bal);
+
+            //----------------------------
+            Console.WriteLine("Питання 4\n скільки буде 4+4*4?");
+            Console.WriteLine("a)16\nb)20\nc)25\nd)16 ");
+            vidpovid = (Console.ReadLine());
+            if (vidpovid == "b") bal += 1;
+            Console.WriteLine(bal);
 
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Тест на тему: 'Якась тема' ");
+            Console.WriteLine("Тест на тему: 'Який у мене рівень математики?' ");
             Console.WriteLine("Розпочати тест нажміть 1,\nВийти з програми натисніть 0\nПереглянути рейтинг натисніть #");
-            string a;
-            test();
+            string a = (Console.ReadLine());
+            if (a == "1") test();
+            if (a == "0") System.Environment.Exit(-1);
+            if (a == "#") Console.WriteLine("Рейтинг скоро буде");
+            else
+                Console.WriteLine("paka");
             Console.ReadLine();
         }
     }
