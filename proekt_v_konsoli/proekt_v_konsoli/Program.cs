e0f394995ffdc775049a9f487ebdf7d7bca5ec88
dimalenser <dima.neuron@gmail.com>
2019-11-08 12:44:40 +0200

    створив проект
    покішо в консолі, коли буде повністю готовий проект в консолі, то тоді я приступлю до перенесення пректу на віконний додаток

---
+++ proekt_v_konsoli/proekt_v_konsoli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proekt_v_konsoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
